95ae027b6cef98874de243d2396886197399eba5
benjaminsssss <be.mariny@duocuc.cl>
2025-09-14 13:58:52 -0300

    estructura del proyecto 4

---
+++ tu_recorrido/lib/utils/colores.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Coloressito {
+  // estos son los colores principales de la app
+  static const Color primary = Color(0xFF6C63FF);
+  static const Color secondary = Color(0xFFFF6584);
+
+
+  //aqui esta los colores de fondo de la app
+
+  static const Color background = Color(0xFFF5F5F5);
+  
+}
