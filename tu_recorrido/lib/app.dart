04fe35b9cee212c04561910c618d823c916aeb26
alejandroka9 <127995663+alejandroka9@users.noreply.github.com>
2025-10-15 21:56:20 -0300

    fix(app): evita tr() en MaterialApp.title para prevenir warning de carga temprana de i18n

--- tu_recorrido/lib/app.dart
+++ tu_recorrido/lib/app.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: tr('app_title'),
+      title: 'Tu Recorrido',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       localizationsDelegates: context.localizationDelegates,
